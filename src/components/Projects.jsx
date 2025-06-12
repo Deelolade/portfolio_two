27a32a105fbb5b0c61820c62579dd093a1478c5f
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-06-12 08:48:07 +0100

    done with the project section just images and animation remaining

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,32 +1,46 @@
-import React from 'react'
-import BgImage from "./Background"
+const Project =({header, description, buttonOne, buttonTwo})=>{
+    return(
+        <>
+            <section className=' mb-12 max-w-5xl mx-auto border border-3 border-gray-300 rounded-xl p-8'>
+                <div className=" flex items-center flex-col">
+                    <h1 className="text-4xl font-bold text-center mb-4">{header}</h1>
+                    <p className=" max-w-2xl text-center text-lg text-gray-700">{description} </p>
+                    <div className=" flex  gap-3 mt-4">
+                        <button className='px-3 py-2 bg-black font-semibold text-sm text-white rounded-lg'>{buttonOne}</button>
+                        <button className='px-3 py-2 border font-semibold text-sm text-gray-700 rounded-lg'>{buttonTwo}</button>
+                    </div>
+                </div>
+                <div className="">
+
+                </div>
+            </section>
+        </>
+    )
+}
 
 const Projects = () => {
-    return (
-        <div className='min-h-screen min-w-screen relative bg-gray-300'>
-            <div className="max-w-[1500px]  relative   h-screen mx-auto flex justify-between items-center">
-                <h1 className='text-7xl font-bold text-center'>My Projects.</h1>
-                <div className="  flex md:flex-row flex-col pr-40 gap-10 h-[65%] items-center overflow-x-auto scroll-smooth scrollbar-hide md:w-[65%] whitespace-nowrap transition ">
-                    <div className=" bg-red-400 ">
-                        <div className="card w-[35vh] h-[30vh] border border-gray-200 rounded-2xl transform transition-transform duration-300 origin-center hover:scale-y-150"> </div>
-                    </div>
-                    <div className=" bg-red-400 ">
-                        <div className="card w-[35vh] h-[30vh] border border-gray-200 rounded-2xl transform transition-transform duration-300 origin-center hover:scale-y-150"> </div>
-                    </div>
-                    <div className=" bg-red-400 ">
-                        <div className="card w-[35vh] h-[30vh] border border-gray-200 rounded-2xl transform transition-transform duration-300 origin-center hover:scale-y-150"> </div>
-                    </div>
-                    <div className=" bg-red-400 ">
-                        <div className="card w-[35vh] h-[30vh] border border-gray-200 rounded-2xl transform transition-transform duration-300 origin-center hover:scale-y-150"> </div>
-                    </div>
-                    <div className=" bg-red-400 ">
-                        <div className="card w-[35vh] h-[30vh] border border-gray-200 rounded-2xl transform transition-transform duration-300 origin-center hover:scale-y-150"> </div>
-                    </div>
-                    <div className="absolute right-0 top-0 h-full w-40 backdrop-blur-3xl bg-gradient-to-l  rounded-l-3xl from-indigo-200/10 to-indigo-400/0 pointer-events-none z-10" />
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className='min-h-screen min-w-screen relative  py-12'>
+        <Project header="QuickGig Freelance Marketplace"
+        description='QuickGig is a mordern freelance marketplace MVP designed to streamline short-term gig hiring. Built for speed and usability. It connects clients with Freelancers in realtime.'
+        buttonOne="Learn More"
+        buttonTwo="Live Demo"
+        />
+        <Project header="QuickGig Freelance Marketplace"
+        description='QuickGig is a mordern freelance marketplace MVP designed to streamline short-term gig hiring. Built for speed and usability. It connects clients with Freelancers in realtime.'
+        buttonOne="Learn More"
+        buttonTwo="Live Demo"
+        /><Project header="QuickGig Freelance Marketplace"
+        description='QuickGig is a mordern freelance marketplace MVP designed to streamline short-term gig hiring. Built for speed and usability. It connects clients with Freelancers in realtime.'
+        buttonOne="Learn More"
+        buttonTwo="Live Demo"
+        /><Project header="QuickGig Freelance Marketplace"
+        description='QuickGig is a mordern freelance marketplace MVP designed to streamline short-term gig hiring. Built for speed and usability. It connects clients with Freelancers in realtime.'
+        buttonOne="Learn More"
+        buttonTwo="Live Demo"
+        />
+    </div>
+  )
 }
 
 export default Projects
