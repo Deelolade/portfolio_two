b37e28cf0e1bb553aa9d285f3ca4434f0679179e
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-06-13 08:12:09 +0100

    done with the hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,78 +1,94 @@
-import React, { useEffect, useRef } from 'react'
-import gsap from 'gsap';
-import resume from "../../public/Deelolade.pdf"
-import { useGSAP } from '@gsap/react';
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import resume from "/Users/oluwanishola_habeeb/Desktop/react_gsap/Deelolade.pdf";
+import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 const HeroSection = () => {
-  gsap.registerPlugin(ScrollTrigger) 
+  gsap.registerPlugin(ScrollTrigger);
   const containerRef = useRef(null);
-  useGSAP(()=>{
-    const tl = gsap.timeline();
-    tl.from(".hero-text",{
-      y: "-300",
-          opacity: 0,
-          filter:"blur(5px)",
-          stagger: {
-            each :0.2
-          },
-          duration: 0.7,
-          ease: "power2.out",
-    })
-    ScrollTrigger.matchMedia({
-    "(min-width: 768px)": ()=>{
-      const leaveTl = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".hero-text",
-          start: "  top center ",   // when top of .hero-text hits center of viewport
-          end: "bottom top",     // when bottom of .hero-text hits top of viewport
-          scrub: true,
-        }
-      });
-      
-      leaveTl.to(".hero-text", {
-        filter: "blur(5px)",
-        opacity: 1,
+  useGSAP(
+    () => {
+      const tl = gsap.timeline();
+       // Entrance animation
+  tl.set(".hero-text", {
+    filter: "blur(5px)",
+  });
+      tl.from(".hero-text", {
+        y: "-300",
+        opacity: 0,
+        filter: "blur(0px)",
         stagger: {
-          each :0.3
+          each: 0.2,
         },
-        duration: 0.9,
+        duration: 0.7,
         ease: "power2.out",
+      });
+      ScrollTrigger.matchMedia({
+        "(min-width: 768px)": () => {
+          const leaveTl = gsap.timeline({
+            scrollTrigger: {
+              trigger: ".hero-text",
+              start: "  top center ", // when top of .hero-text hits center of viewport
+              end: "bottom top", // when bottom of .hero-text hits top of viewport
+              scrub: true,
+            },
           });
-    }  
-    })
 
-  },{scope:containerRef})
-
+          leaveTl.to(".hero-text", {
+            y: 1,
+            filter: "blur(5px)",
+            opacity: 1,
+            stagger: {
+              each: 0.3,
+            },
+            duration: 0.9,
+            ease: "power2.out",
+          });
+        },
+      });
+    },
+    { scope: containerRef }
+  );
 
   return (
-    <div className='h-screen flex justify-center  items-center md:px-4'>
-      <div className=" text-left max-w-5xl xxs:mt-16" ref={containerRef} >
-        <h1 className='hero-text xxs:text-3xl xs:text-4xl text-6xl font-bold ' >Hire Deelolade.</h1>
+    <div className="min-h-screen flex justify-center  items-center md:px-4">
+      <div className=" text-left max-w-5xl xxs:mt-16" ref={containerRef}>
+        <h1 className="hero-text xxs:text-3xl xs:text-4xl text-6xl font-bold ">
+          Hire Deelolade.
+        </h1>
         <p className=" hero-text xxs:text-3xl xs:text-4xl text-6xl font-bold ">
           The Best Full-Stack Developer.
         </p>
         <p className="max-w-3xl xxs:text-[15px] xxs:text-left xs:text-lg hero-text text-lg sm:text-xl mt-2 text-gray-400">
-        I’m a freelance <b className='hover:text-black transition'>full-stack developer</b> dedicated to building modern, scalable <b  className='hover:text-black transition'>web applications</b>. <br /> I specialize in creating smooth user experiences and performance-optimized digital products for <b className='hover:text-black transition'>startups</b> and <b className='hover:text-black transition'>businesses</b> that value quality and speed.
+          I build fast, scalable <b className="underline">web applications</b>{" "}
+          using <b className="">React</b>,<b className="">Next.js</b>,{" "}
+          <b className="">Node.js</b>,<b className=""> MongoDB</b>, and{" "}
+          <b className="">Tailwind CSS</b>. I trained at{" "}
+          <b className="underline">New Horizons Nigeria</b>, where I developed a
+          strong foundation in web development and started building real
+          solutions for <b>startups</b> and <b>small businesses</b>. When I’m
+          not coding, you’ll probably find me sewing,{" "}
+          <b className="underline">my creative hobby</b> that sharpens my focus
+          and attention to detail.
         </p>
         {/* CTA Buttons */}
-        <div  className="hero-text  mt-8 flex flex-wrap gap-4 justify-start">
+        <div className="hero-text  mt-8 flex flex-wrap gap-4 justify-start">
           <a
             href={resume}
-            className="bg-gray-700 text-white xxs:px-3 xxs:py-2 px-6 py-3 rounded-md text-lg font-medium hover:bg-black transition"
+            className="bg-gray-700 text-white xxs:px-3 xs:py-2 px-6 py-2 rounded-md text-lg font-medium hover:bg-black transition"
           >
             View My Work
           </a>
           <a
             href="mailto:habeeboluwanishola13@gmail.com"
-            className="border border-black text-gray-700 px-6 xxs:py-2 py-3 rounded-md text-lg font-medium hover:bg-black hover:text-white transition"
+            className="border border-black text-gray-700 px-6 xs:py-2 py-2 rounded-md text-lg font-medium hover:bg-black hover:text-white transition"
           >
             Let's Talk
           </a>
         </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
